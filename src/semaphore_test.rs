d92c99acb31c27ec6d2c041f5d006bf19c51d900
Ryo Suzuki <ryo.s12330@gmail.com>
2023-06-23 01:37:59 +0900

    semapho

---
+++ src/semaphore_test.rs
@@ -0,0 +1,39 @@
+mod semaphore;
+use semaphore::Semaphore;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::thread;
+
+const NUM_LOOP: usize = 100000;
+const NUM_THREADS: usize = 8;
+const SEM_NUM: isize = 4;
+
+static mut CNT: AtomicUsize = AtomicUsize::new(0);
+
+fn main() {
+    let mut v = Vec::new();
+    let sem = Arc::new(Semaphore::new(SEM_NUM));
+
+    for i in 0..NUM_THREADS {
+        let s = sem.clone();
+        let t = thread::spawn(move || {
+            for _ in 0..NUM_LOOP {
+                s.wait();
+
+                unsafe { CNT.fetch_add(1, Ordering::SeqCst) };
+                let n = unsafe { CNT.load(Ordering::SeqCst) };
+                println!("semaphore: i = {}, CNT = {}", i, n);
+                assert!((n as isize) <= SEM_NUM);
+                unsafe { CNT.fetch_sub(1, Ordering::SeqCst) };
+
+                s.post();
+            }
+        });
+
+        v.push(t);
+    }
+
+    for t in v {
+        t.join().unwrap();
+    }
+}
